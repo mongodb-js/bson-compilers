0651aca1c5c62351683ae04816a3a2d1e30dd1b6
Alena Khineika <>
2018-03-20 14:56:49 +0100

    Created test for java symbol

--- test/helpers.js
+++ test/helpers.js
@@ -15,7 +15,7 @@ const compile = {
 const unsupported = {
   java: [
     'RegExp', 'BsonRegExp', 'Decimal128', 'Timestamp',
-    'Number', 'DateObject', 'Object', 'Symbol'
+    'Number', 'DateObject', 'Object'
   ],
   python: ['RegExp', 'BsonRegExp', 'DBRef', 'Decimal128', 'Timestamp']
 };

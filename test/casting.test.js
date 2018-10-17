ad70ab160a507ae21d0697ef26b5b2a67b4cb121
aherlihy <anna.herlihy@10gen.com>
2018-10-17 16:35:41 -0400

    test refactor done :)

--- test/casting.test.js
+++ test/casting.test.js
@@ -1,55 +1,68 @@
 /* eslint new-cap: 0 */
 const path = require('path');
+const fs = require('fs');
 const chai = require('chai');
 const expect = chai.expect;
 
 const { getTree } = require('../');
 const yaml = require('js-yaml');
-const { readYAML } = require('./helpers');
 
-const runTest = () => {
-  describe('Casting tests', () => {
-    const testpath = path.join(__dirname, 'yaml', 'edge-cases', 'casting.yaml');
-    const tests = readYAML(testpath).tests.all;
-    for (const test of tests) {
-      for (const input of Object.keys(test.input)) {
-        for (const output of Object.keys(test.output)) {
-          const Visitor = require(`../codegeneration/${input}/Visitor`);
-          const Generator = require(`../codegeneration/${output}/Generator`);
-          const symbols = require(`../lib/symbol-table/${input}to${output}`);
+const modes = process.env.MODE ? process.env.MODE.split(',') : [];
 
-          const Transpiler = Generator(Visitor);
-          const transpiler = new Transpiler();
-          const doc = yaml.load(symbols);
-          transpiler.Types = Object.assign({}, doc.BasicTypes, doc.BsonTypes);
-          transpiler.Symbols = Object.assign(
-            {
-              TestFunc: {
-                callable: 2,
-                args: [],
-                template: null,
-                argsTemplate: null,
-                id: 'TestFunc',
-                type: null
-              }
-            },
-            doc.BsonSymbols, doc.NativeSymbols);
-          transpiler.Syntax = doc.Syntax;
-          transpiler.SYMBOL_TYPE = doc.SymbolTypes;
-          describe(`from ${input} to ${output}`, () => {
-            it(test.description, () => {
-              if (test.input[input].args) {
-                transpiler.Symbols.TestFunc.args = test.input[input].args.map((t) => {
-                  return t.map((k) => ( k !== null ? transpiler.Types[k] : k ));
-                });
-              }
-              const str = getTree[input](test.input[input].code);
-              expect(transpiler.start(str)).to.equal(test.output[output]);
-            });
+const readYAML = (filename) => {
+  let parseResult;
+  try {
+    parseResult = yaml.load(fs.readFileSync(filename));
+  } catch (err) {
+    err.message = `${filename}: ${err.message}`;
+    throw err;
+  }
+  return parseResult;
+};
+
+describe('Casting tests', () => {
+  if (modes.length > 0 && modes.indexOf('casting') === -1) {
+    return;
+  }
+  const testpath = path.join(__dirname, 'yaml', 'edge-cases', 'casting.yaml');
+  const tests = readYAML(testpath).tests.all;
+  for (const test of tests) {
+    for (const input of Object.keys(test.input)) {
+      for (const output of Object.keys(test.output)) {
+        const Visitor = require(`../codegeneration/${input}/Visitor`);
+        const Generator = require(`../codegeneration/${output}/Generator`);
+        const symbols = require(`../lib/symbol-table/${input}to${output}`);
+
+        const Transpiler = Generator(Visitor);
+        const transpiler = new Transpiler();
+        const doc = yaml.load(symbols);
+        transpiler.Types = Object.assign({}, doc.BasicTypes, doc.BsonTypes);
+        transpiler.Symbols = Object.assign(
+          {
+            TestFunc: {
+              callable: 2,
+              args: [],
+              template: null,
+              argsTemplate: null,
+              id: 'TestFunc',
+              type: null
+            }
+          },
+          doc.BsonSymbols, doc.NativeSymbols);
+        transpiler.Syntax = doc.Syntax;
+        transpiler.SYMBOL_TYPE = doc.SymbolTypes;
+        describe(`from ${input} to ${output}`, () => {
+          it(test.description, () => {
+            if (test.input[input].args) {
+              transpiler.Symbols.TestFunc.args = test.input[input].args.map((t) => {
+                return t.map((k) => ( k !== null ? transpiler.Types[k] : k ));
+              });
+            }
+            const str = getTree[input](test.input[input].code);
+            expect(transpiler.start(str)).to.equal(test.output[output]);
           });
-        }
+        });
       }
     }
-  });
-};
-// runTest();
+  }
+});
--- test/run-yaml.test.js
+++ test/run-yaml.test.js
@@ -3,37 +3,30 @@ const path = require('path');
 const fs = require('fs');
 const chai = require('chai');
 const expect = chai.expect;
+const yaml = require('js-yaml');
 
-const { readYAML, readJSON } = require('./helpers');
 const transpiler = require('../index');
 
 const outputLanguages = process.env.OUTPUT ? process.env.OUTPUT.split(',') : [ 'csharp', 'python', 'java', 'javascript', 'shell'];
 const inputLanguages = process.env.INPUT ? process.env.INPUT.split(',') : [ 'shell', 'javascript', 'python' ];
 const modes = process.env.MODE ? process.env.MODE.split(',') : [];
 
-const testpath2 = path.join(__dirname, 'yaml', 'error');
-const total = {};
-const yaml = readJSON(path.join(testpath2, 'javascript', 'unimplemented-error.json')).tests;
-for (const i of Object.keys(yaml)) {
-  total[i] = [];
-  for (const j of yaml[i]) {
-    const doc = {
-      description: j.description,
-      input: {
-        javascript: j.query
-      },
-      errorCode: j.errorCode
-    };
-    total[i].push(doc);
-  }
-}
-// console.log(JSON.stringify(total));
-
 const skipType = [];
+
+const readYAML = (filename) => {
+  let parseResult;
+  try {
+    parseResult = yaml.load(fs.readFileSync(filename));
+  } catch (err) {
+    err.message = `${filename}: ${err.message}`;
+    throw err;
+  }
+  return parseResult;
+};
 
 const testpath = path.join(__dirname, 'yaml');
 fs.readdirSync(testpath).map((file) => {
-  if (file === 'edge-cases' || file === 'error') {
+  if (file === 'edge-cases') {
     return; // Ignore edge case tests, they have their own runners
   }
   const mode = file.replace('.yaml', '');

2f4f5e5e7f4a026384ce2f1dc20b21672eac5dac
aherlihy <anna.herlihy@10gen.com>
2018-10-27 23:29:01 -0400

    move imports to codegenvisitor

--- codegeneration/python/Visitor.js
+++ codegeneration/python/Visitor.js
@@ -51,50 +51,7 @@ module.exports = (CodeGenerationVisitor) => class Visitor extends CodeGeneration
     this.visitAugassign =
       this.unimplemented;
   }
-  deepCopyRequiredImports() {
-    const copy = Object.assign({}, this.requiredImports);
-    [300, 301, 302, 303, 304, 305, 306].forEach((i) => {
-      copy[i] = Array.from(this.requiredImports[i]);
-    });
-    return copy;
-  }
 
-  /**
-   * As code is generated, any classes that require imports are tracked in
-   * this.Imports. Each class has a "code" that is defined in the symbol table.
-   * The imports are then generated based on the output language templates.
-   *
-   *  @return {String} - The list of imports in the target language.
-   */
-  getImports() {
-    const importTemplate = this.Imports.import.template ?
-      this.Imports.import.template :
-      (s) => (
-        Object.values(s)
-          .filter((a, i) => (Object.values(s).indexOf(a) === i))
-          .join('\n')
-      );
-    // Remove empty arrays because js [] is not falsey :(
-    [300, 301, 302, 303, 304, 305, 306].forEach(
-      (i) => {
-        if (i in this.requiredImports && this.requiredImports[i].length === 0) {
-          this.requiredImports[i] = false;
-        }
-      });
-    const imports = Object.keys(this.requiredImports)
-      .filter((code) => {
-        return (
-          this.requiredImports[code] &&
-          this.Imports[code] &&
-          this.Imports[code].template
-        );
-      })
-      .reduce((obj, c) => {
-        obj[c] = this.Imports[c].template(this.requiredImports[c]);
-        return obj;
-      }, {});
-    return importTemplate(imports);
-  }
   /**
    * Selectively visits children of a node.
    *

1681d7d55cee857fcfbf74fda022b7ddf50fdf5b
mcasimir <maurizio.cas@gmail.com>
2021-02-22 16:47:14 +0100

    only download antlr on npm compile

--- download-antlr.js
+++ download-antlr.js
@@ -1,5 +1,18 @@
-// eslint-disable-next-line no-console
-console.log('downloading antlr');
+/* eslint-disable no-console */
+/* eslint-disable no-sync */
 
-require('https').get('https://www.antlr.org/download/antlr-4.7.2-complete.jar',
-  (r) => r.pipe(require('fs').createWriteStream('antlr-4.7.2-complete.jar')));
+const fs = require('fs');
+const path = require('path');
+const https = require('https');
+
+const downloadUrl = 'https://www.antlr.org/download/antlr-4.7.2-complete.jar';
+const outputFile = path.resolve(__dirname, 'antlr-4.7.2-complete.jar');
+
+if (fs.existsSync(outputFile)) {
+  console.info(outputFile, 'found, skipping download');
+  process.exit(0);
+}
+
+console.info('downloading antlr');
+
+https.get(downloadUrl, (response) => response.pipe(fs.createWriteStream(outputFile)));
